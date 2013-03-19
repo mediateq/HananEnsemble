c5d52a0246a090fba2a96eff0a41de2038f4dbad
etp1711 <etp1711@gmail.com>
2013-03-20 01:51:33 +0430

    add  first files

---
+++ inc/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1">
+	<meta charset="UTF-8">
+	<title></title>
+
+	<link rel="stylesheet" href="./1styles.css" />
+	<link rel="stylesheet" href="./style.css" />
+
+</head>
+<body>
+	
