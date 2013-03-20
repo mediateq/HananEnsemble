a80c7e11c87202303133d5500ef7cf17e2357b56
etp1711 <etp1711@gmail.com>
2013-03-20 11:49:53 +0430

    add my pics and my files

--- index.php
+++ index.php
@@ -1,5 +1,202 @@
-<?php include('./inc/header.php') ?>
+<!DOCTYPE html>
+<html lang="en"><meta style="visibility: hidden !important; display: block !important; width: 0px !important; height: 0px !important; border-style: none !important;"></meta><head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/jquery.fancybox-1.3.4.css" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Lora:400,700" rel="stylesheet" type="text/css">
+<link href="css/style.css" rel="stylesheet" type="text/css">
+<link href="css/grid.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+<script type="text/javascript" async="" src="http://www.google-analytics.com/ga.js"></script><script src="js/jquery.min.js" type="text/javascript"></script>
+<script src="js/scripts.js" type="text/javascript"></script><script type="text/javascript" src="js/switcher.js"></script><script type="text/javascript" src="js/uCarousel.js"></script><script type="text/javascript" src="js/forms.js"></script><script type="text/javascript" src="js/jquery.fancybox-1.3.4.pack.js"></script><script type="text/javascript" src="js/uScroll.js"></script><script type="text/javascript" src="js/sImg.js"></script>
+<!--[if lt IE 8]>
+  <div class='alc' style=' clear: both; text-align:center; position: relative; z-index:9999;'>
+    <a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+      <img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+   </a>
+ </div>
+<![endif]-->
+<!--[if lt IE 9]><script src="js/html5.js" type="text/javascript"></script><![endif]-->
+<!--[if IE]><link href="css/ie_style.css" rel="stylesheet" type="text/css" /><![endif]-->
+</head>
+<body style="overflow: auto; ">
+<div id="gspinner" class="spinner" style="display: none; "></div>
+<div id="glob">	
+	<div id="_main">
+		<div id="splash-gallery" style="width: 1018px; height: 735px; ">
+			<img src="images/splash-gallery-6.jpg" alt="" style="position: absolute; left: 0px; top: 0px; ">
+			<ul>
+				<li class=""><a href="images/splash-gallery-1.jpg"></a></li>
+				<li><a href="images/splash-gallery-2.jpg"></a></li>
+				<li><a href="images/splash-gallery-3.jpg"></a></li>
+				<li><a href="images/splash-gallery-4.jpg"></a></li>
+				<li><a href="images/splash-gallery-5.jpg"></a></li>
+				<li class="active"><a href="images/splash-gallery-6.jpg"></a></li>
+			</ul>
+		<span class="spinner" style="display: none; "></span></div>
+		<h1 style="top: 93px; "><a href="#!/splash"><img src="images/logo.png" alt=""></a></h1>
+		<nav style="top: 89px; ">
+			<ul>
+				<li class=""><a href="#!/main">main</a><img src="images/nav-bg.png" alt=""></li>
+				<li class=""><a href="#!/about_us">about us</a><img src="images/nav-bg.png" alt=""></li>
+				<li class=""><a href="#!/gallery">gallery</a>
+					<ul style="opacity: 0; display: none; ">
+						<li><a href="#!/readmore">gallery1</a></li>
+						<li><a href="#!/readmore">gallery2</a></li>
+						<li><a href="#!/readmore">gallery3</a></li>
+					</ul>
+					<img src="images/nav-bg.png" alt="">
+				</li>
+				<li class="active"><a href="#!/contacts">contacts</a><img src="images/nav-bg.png" alt=""></li>
+			</ul>			
+		</nav>
+		<article id="content" class="container_24" style="bottom: 0px; ">
+			<ul>
+				<li id="main" style="display: none; ">
+					<div class="grid_9 suffix_2 alpha border-right">
+						<div class="img-box-set">
+							<dl class="border-bottom">
+								<dt><img src="images/main-img-1.jpg" alt=""></dt>
+								<dd>
+									<h2>About Thomas</h2>
+									Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque.
+								</dd>
+							</dl>
+							<dl>
+								<dt><img src="images/main-img-2.jpg" alt=""></dt>
+								<dd>
+									<h2>About Lora</h2>
+									Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit.
+ varius mi. Cum sociis natoque penatibus et magnis dis parturient.
+								</dd>
+							</dl>
+						</div>						
+					</div>
+					<div class="grid_12 prefix_1 omega">
+							<img src="images/main-img-3.jpg" class="blo" alt="">
+							<h2>Our Love Story</h2>
+							<div class="scroll" style="position: relative; overflow: hidden; ">
+								<div style="overflow: hidden; position: relative; top: 0px; ">
+									<p>Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. </p>
+									<p>Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. </p>
+									Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. 
+								</div>
+							<track class="_track _inside _axis-y" style="display: block; position: absolute; "><shuttle class="_shuttle" style="display: block; position: absolute; left: 0px; top: 0px; cursor: auto; "></shuttle></track></div>							
+						</div>
+				</li>
+				<li id="about_us" style="display: none; ">
+					<div class="grid_24 alpha omega">
+						<h2>How We've Met</h2>
+						<dl class="img-box">
+							<dt><img src="images/about-img-1.jpg" alt=""></dt>
+							<dd>
+								<p>Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum 1998 sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maes tristique orci ac sem. Duis ultricies pharetra magna. Donec 2001 accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. </p>
+								
+Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum 2007 sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer 2012 adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. 
+							</dd>
+						</dl>
+					</div>
+				</li>
+				<li id="gallery" style="display: none; ">
+					<div class="carousel" style="position: relative; width: 952px; height: 332px; overflow: hidden; ">
+						<ul style="position: relative; width: 3808px; height: 332px; ">
+							<li style="display: block; position: absolute; left: 0px; top: 0px; " class="active"><a href="images/image_blank_white.jpg"><img src="images/gallery-img-1.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 238px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-2.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 476px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-3.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 714px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-4.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 952px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-5.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 1190px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-6.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 1428px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-7.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 1666px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-8.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 1904px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-1.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 2142px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-2.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 2380px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-3.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 2618px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-4.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 2856px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-5.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 3094px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-6.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 3332px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-7.jpg" alt=""></a></li>
+							<li style="display: block; position: absolute; left: 3570px; top: 0px; "><a href="images/image_blank_white.jpg"><img src="images/gallery-img-8.jpg" alt=""></a></li>
+						</ul>
+					</div>
+					<div class="btns">	
+						<a href="#" data-type="prevPage" class="gallery-btn"></a>
+						<a href="#" data-type="nextPage" class="gallery-btn"></a>
+					</div>
+				</li>
+				<li id="contacts" style="display: list-item; ">
+					<div class="grid_8 alpha suffix_1 border-right">
+						<h2>Find Us</h2>
+						<div class="google_map blo"><iframe class="blo" width="310" height="150" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Brooklyn,+New+York,+NY,+United+States&amp;aq=0&amp;sll=37.0625,-95.677068&amp;sspn=61.282355,146.513672&amp;ie=UTF8&amp;hq=&amp;hnear=Brooklyn,+Kings,+New+York&amp;ll=40.649974,-73.950005&amp;spn=0.01628,0.025663&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe></div>
+						<dl class="address">
+							<dt>8901 Marmora Road, Glasgow, D04 89GR. </dt>
+							<dd><span>Freephone:</span>+1 800 559 6580</dd>
+							<dd><span>Telephone:</span>+1 959 603 6035</dd>
+							<dd><span>FAX:</span>+1 504 889 9898</dd>
+							<dd><span>E-mail: <a href="#" class="und nocolor">mail@demolink.org</a></span></dd>
+						</dl>
+					</div>
+					<div class="grid_14 prefix_1 omega">
+						<h2>Get In Touch</h2>
+						<form id="form1">
+						  <div class="success" style="display: none; "> Contact form submitted!<br>
+							<strong>We will be in touch soon.</strong> </div>
+						  <fieldset>
+							<label class="name">
+							  <input type="text" value="Name:">
+							  <span class="error" style="display: none; ">*This is not a valid name.</span> <span class="empty" style="display: none; ">*This field is required.</span> </label>
+							<label class="email">
+							  <input type="email" value="E-mail:">
+							  <span class="error" style="display: none; ">*This is not a valid email address.</span> <span class="empty" style="display: none; ">*This field is required.</span> </label>
+							<label class="phone">
+							  <input type="tel" value="Phone:">
+							  <span class="error" style="display: none; ">*This is not a valid phone number.</span> <span class="empty" style="display: none; ">*This field is required.</span> </label>
+							<label class="message">
+							  <textarea>Message:</textarea>
+							  <span class="error" style="display: none; ">*The message is too short.</span> <span class="empty" style="display: block; ">*This field is required.</span> </label>
+							<br class="clear">
+							<div class="btns"><a href="#" class="rdmr v2 anim-underline" data-type="submit">Send Message</a></div>
+						  </fieldset>
+						</form>
+					</div>
+				</li>
+				<li id="privacy" style="display: none; ">
+					<div class="grid_24 alpha omega">
+						<h2>Privacy Policy</h2>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus.</p>
+						<p>Quisque nulla. Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magna, hendrerit sit amet, tincidunt ac, viverra sed, nulla. Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar. Proin ullamcorper urna et felis. Vestibulum iaculis lacinia est. Proin dictum elementum velit. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque sed dolor. Aliquam congue fermentum nisl. </p>
+						E-mail: <a href="#">info@demolink.org</a>
+					</div>
+				</li>
+				<li id="readmore" style="display: none; ">
+					<div class="grid_24 alpha omega">
+						<h2>Read More</h2>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Praesent vestibulum molestie lacus. Aenean nonummy hendrerit mauris. Phasellus porta. Fusce suscipit varius mi. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla dui. Fusce feugiat malesuada odio. Morbi nunc odio, gravida at, cursus nec, luctus a, lorem. Maecenas tristique orci ac sem. Duis ultricies pharetra magna. Donec accumsan malesuada orci. Donec sit amet eros. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Mauris fermentum dictum magna. Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus.</p>
+						<p>Quisque nulla. Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel metus. Nulla facilisi. Aenean nec eros. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Suspendisse sollicitudin velit sed leo. Ut pharetra augue nec augue. Nam elit magna, hendrerit sit amet, tincidunt ac, viverra sed, nulla. Donec porta diam eu massa. Quisque diam lorem, interdum vitae, dapibus ac, scelerisque vitae, pede. Donec eget tellus non erat lacinia fermentum. Donec in velit vel ipsum auctor pulvinar. Proin ullamcorper urna et felis. Vestibulum iaculis lacinia est. Proin dictum elementum velit. Fusce euismod consequat ante. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque sed dolor. Aliquam congue fermentum nisl. </p>
+					</div>
+				</li>
+			</ul>
+		</article>
+	</div>
+	<div class="splash-btns">
+		<a href="#" class="prev" style="opacity: 0.6; display: none; "></a>
+		<a href="#" class="next" style="opacity: 0.6; display: none; "></a>
+	</div>
+	<footer><pre class="privacy und nocolor">thomas &amp; lora (c) 2012  |  <a href="#!/privacy">Privacy Policy</a></pre><!-- {%FOOTER_LINK} --></footer>
+</div>
+<!--coded by pleazkin-->
+
+<!--LIVEDEMO_00 -->
+
+<script type="text/javascript">
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7078796-5']);
+  _gaq.push(['_trackPageview']);
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();</script>
 
 
-
-<?php include('./inc/footer.php') ?>
+<div id="fancybox-tmp"></div><div id="fancybox-loading"><div></div></div><div id="fancybox-overlay"></div><div id="fancybox-wrap"><div id="fancybox-outer"><div class="fancybox-bg" id="fancybox-bg-n"></div><div class="fancybox-bg" id="fancybox-bg-ne"></div><div class="fancybox-bg" id="fancybox-bg-e"></div><div class="fancybox-bg" id="fancybox-bg-se"></div><div class="fancybox-bg" id="fancybox-bg-s"></div><div class="fancybox-bg" id="fancybox-bg-sw"></div><div class="fancybox-bg" id="fancybox-bg-w"></div><div class="fancybox-bg" id="fancybox-bg-nw"></div><div id="fancybox-content"></div><a id="fancybox-close"></a><div id="fancybox-title"></div><a href="javascript:;" id="fancybox-left"><span class="fancy-ico" id="fancybox-left-ico"></span></a><a href="javascript:;" id="fancybox-right"><span class="fancy-ico" id="fancybox-right-ico"></span></a></div></div></body></html>
