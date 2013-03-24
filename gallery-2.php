881bc75195af3f985763a98004fe0f6325711d66
etp1711 <etp1711@gmail.com>
2013-03-24 23:57:20 +0430

    add gallery 2

--- gallery-2.php
+++ gallery-2.php
@@ -71,6 +71,7 @@
 			<div class="links">
 				<a href="./gallery.php">1</a>
 				<a href="./gallery-2.php">2</a>
+				<a href="./gallery-3.php">3</a>
 			</div>
 			<div class="badboy"></div>
 		</div>
