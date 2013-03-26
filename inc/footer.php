91b59c20f3d330c607d535af724d3de8c44d6acf
etp1711 <etp1711@gmail.com>
2013-03-26 13:42:54 +0430

    add slide files logo new

--- inc/footer.php
+++ inc/footer.php
@@ -8,17 +8,19 @@
 					<div class="train">
 						<?php
 						$path= './images/slidepic';
+						$large_img = './images/slidepic';
 						$files= scandir($path);
 						$len= 0;
 						foreach ($files as $file) {
 							$exp= explode('.',$file);
 							$type= end($exp);
 							if ($type == 'jpg'){
-								echo "<img src='$path/$file'>";
+								echo "<a href='$large_img/$file'><img src='$path/$file'></a>";
 								$len++;
 							}
 						}
 					?>
+					</a>
 					</div>
 				</div>
 		</div>
