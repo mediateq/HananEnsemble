933128387a054ca8b12568f4ca878bc715efed88
etp1711 <etp1711@gmail.com>
2013-04-05 17:36:07 +0430

    add some date

--- contact.php
+++ contact.php
@@ -5,9 +5,9 @@
 			<div class="rightcontact">
 				<h1>ارسال پیام</h1>
 				<form action="mail.php" method="post" target="ifrm">
-					<input type="text" placeholder=": نام" class="name">
-					<input type="text" placeholder=": ايميل" class="email">
-					<textarea placeholder=": پیام" class="messege"></textarea>
+					<input type="text" placeholder="نام :" class="name">
+					<input type="text" placeholder="ايميل :" class="email">
+					<textarea placeholder="پیام :" class="messege"></textarea>
 					<input type="submit" value="ارسال" class="but"/>
 				</form>
 				<iframe id='ifrm' name='ifrm' src="" frameborder="0" scrolling="no"></iframe>
